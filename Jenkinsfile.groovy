9122b93dd220c92ecc0944b02f9813a19ccf6845
manoj.kumar <manoj.kumar@careem.com>
2017-11-13 11:55:28 +0400

    Stage2 jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,12 +5,14 @@ pipeline {
         pollSCM('H/15 * * * *')
     }
 
-    stages {
-        stage("Sample"){
-            steps {
-                sh '''
-                    echo "In stage2 2 branch"
-                '''
+    node {
+        stages {
+            stage("Sample"){
+                steps {
+                    sh '''
+                        echo "In stage2 2 branch"
+                    '''
+                }
             }
         }
     }
