7394ac0221bc5035de2d452d8c464d0fe45ff4fe
manoj.kumar <manoj.kumar@careem.com>
2017-11-13 14:01:17 +0400

    fixing jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,13 +12,8 @@ pipeline {
             stage("Sample"){
                 steps {
                     sh '''
-<<<<<<< HEAD
                         echo "In stage2 2 branch"
-=======
-                        echo "In stage1 branch"
-                        echo "My change for stage1"
->>>>>>> 3c5709dd5ae3e31d117a93469bda788e64bce69e
-                    '''
+                   '''
                 }
             }
         }
